9b42037229e4f1873093be529b8c27d4c25765fb
Giuseppe Terrasi <giuseppe.terrasi@hotmail.it>
2020-02-01 16:27:16 +0100

    Add comments in time.f file

--- time.f
+++ time.f
@@ -1,13 +1,15 @@
 HEX
 
-3F003004 CONSTANT SYS_CLO
+3F003004 CONSTANT SYS_CLO  \ Free runner register address of System Timer
 
+\ NOW ( -- n ) get time passed from startup in microseconds
 : NOW SYS_CLO @ ;
 
 DECIMAL
 
-: USEC ;
-: MSEC 1000 * ;
-: SEC 1000 MSEC * ;
+: USEC ;  \ microseconds
+: MSEC 1000 * ; \ milliseconds
+: SEC 1000 MSEC * ; \ seconds 
 
+\ DELAY ( n -- ) wait for given time passed as input
 : DELAY NOW + BEGIN DUP NOW - 0 <= UNTIL DROP ;
