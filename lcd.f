6e8ab2440b5cf191f2fc7d0d72432008611a216e
Giuseppe Terrasi <giuseppe.terrasi@hotmail.it>
2020-02-09 22:42:47 +0100

    Added new words to lcd.f

--- lcd.f
+++ lcd.f
@@ -25,6 +25,7 @@ HEX
 80 CONSTANT LCDLN1
 C0 CONSTANT LCDLN2
 
+\ LCDSETUP ( -- ) Set LCD pins as output
 : LCDSETUP 
     LCDE  GPFSELOUT!
     LCDRS GPFSELOUT!
@@ -38,13 +39,16 @@ C0 CONSTANT LCDLN2
     LCD7 GPFSELOUT!
 ;
 
-
+\ +LCDE ( -- ) Set LCDE pin to high
 : +LCDE LCDE GPON! ;
 
+\ -LCDE ( -- ) Set LCDE pin to low
 : -LCDE LCDE GPOFF! ;
 
+\ LCDESIGN ( -- ) Send LCDE pin high low signal to LCD
 : LCDESIGN 500 USEC DELAY +LCDE 500 USEC DELAY -LCDE 500 USEC DELAY ;
 
+\ LCDDOFF! ( -- ) Set all LCD pins to low
 : LCDDOFF!
     LCDE GPOFF!
     LCD0 GPOFF!
@@ -57,9 +61,13 @@ C0 CONSTANT LCDLN2
     LCD7 GPOFF!
 ;
 
+\ BITSET ( n1 n2 -- flag ) Check if n2-bit of n1 is set and leaves a status flag into the stack 
 : BITSET AND 0<> ;
+
+\ ?LCDD ( flag n -- flag ) Check flag and set n-pin accordly to flag
 : ?LCDD SWAP IF GPON! ELSE GPOFF! THEN ;
 
+\ LCDWRITE ( n -- ) Write n to LCD in 8 bit mode. If n < 0x100 a command is sent otherwise data are sent.
 : LCDWRITE
     DUP 100 BITSET LCDRS ?LCDD
 
@@ -75,6 +83,7 @@ C0 CONSTANT LCDLN2
     LCDESIGN
 ;
 
+\ LCDWRITE ( n -- ) Write n to LCD in 4 bit mode. If n < 0x100 a command is sent otherwise data are sent.
 : LCDWRITE4
     DUP 100 BITSET LCDRS ?LCDD
     
@@ -95,6 +104,7 @@ C0 CONSTANT LCDLN2
     LCDESIGN
 ;
 
+\ LCDCLR ( -- ) Clear LCD display
 : LCDCLR 1 LCDWRITE ;
 
 : LCDINIT
@@ -105,7 +115,39 @@ C0 CONSTANT LCDLN2
     LCDCLR
 ;
 
+\ LCDSTYPE ( addr n -- ) Print a string to LCD
+: LCDSTYPE OVER + SWAP BEGIN 2DUP <> WHILE DUP c@ 100 + LCDWRITE 1+ REPEAT 2DROP ;
 
-: LCDTYPE OVER + SWAP BEGIN 2DUP <> WHILE dup c@ 100 + LCDWRITE 1+ REPEAT 2DROP ;
+DECIMAL
 
-: LCDMESSAGE LCDWRITE LCDTYPE ;
+\ LCDSTYPE ( n -- ) Print a string rappresentation of a number to LCD
+: LCDNTYPE BEGIN 10 /MOD SWAP 304 + LCDWRITE DUP 0= UNTIL DROP ;
+
+HEX
+
+\ LCDLN ( n -- addr ) Gets the corresponding address of LCD line passed as input 
+: LCDLN 1 = IF LCDLN1 ELSE LCDLN2 THEN ;
+
+\ LCDLN ( n -- addr ) Set DDRAM addres of LCD line where cursor will start to write
+: LCDLN! LCDLN + LCDWRITE ;
+
+\ LCDCURL>R ( -- ) Set cursor move from left to right
+: LCDCURL>R 6 LCDWRITE ;
+
+\ LCDCURL<R ( -- ) Set cursor move from right to left
+: LCDCURL<R 4 LCDWRITE ;
+
+\ LCDSTRING ( addr n1 n2 n3 -- ) Print to LCD line based on n3 the string starting from addr and long n1 from an offset of n2 from the beginning of the line
+: LCDSTRING LCDLN! LCDCURL>R LCDSTYPE ;
+
+\ LCDSTRING ( n1 n2 n3 -- ) Print to LCD line based on n3 the string rappresentation of n1 from an offset of n2 from the beginning of the line
+: LCDNUMBER LCDLN! LCDCURL<R LCDNTYPE ;
+
+\ LCDSPACE write a space to LCD
+: LCDSPACE 120 LCDWRITE ;
+
+DECIMAL
+
+\ LCDLNCLR ( n -- ) Clear LCD line n
+: LCDLNCLR LCDLN! LCDCURL>R 16 0 BEGIN LCDSPACE 1 + 2DUP = UNTIL 2DROP ;
+
